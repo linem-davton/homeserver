9dd49d5a7806ed2cbf55031cdc2988750e7942ca
linem davton <linem.davton@gmail.com>
2025-08-23 04:30:02 +0200

    photo, llm, nextcloud

---
+++ llm/webui_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway --name open-webui -e OLLAMA_BASE_URL=http://host.docker.internal:11434 -v open-webui:/app/backend/data --restart unless-stopped ghcr.io/open-webui/open-webui:main
